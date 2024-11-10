486cffa656c6c793fd0ced03aa73cb4980bb5cf7
mohuynh <mohuynh@users.noreply.github.com>
2024-11-10 18:33:02 +0100

    Update frontend for deployment

--- src/app/services/api.js
+++ src/app/services/api.js
@@ -1,20 +1,24 @@
+
+const backendBaseUrl = "https://www.cefii-developpements.fr/morgane1412/mods-app"; // Prod
+// const backendBaseUrl = "http://localhost:8000/Router.php?REQUEST_URI="; // Dev only
+
 // Table Mods
 export async function getModsList() {
-      const reponse = await fetch("http://localhost:8000/mods");
+      const reponse = await fetch(`${backendBaseUrl}/mods`);
 
       const data = await reponse.json();
       return data;
 }
 
 export async function getMod(modId) {
-      const reponse = await fetch("http://localhost:8000/mods/" + modId);
+      const reponse = await fetch(`${backendBaseUrl}/mods/` + modId);
 
       const data = await reponse.json();
       return data;
 }
 
 export async function deleteMod(accessToken, modId) {
-      const reponse = await fetch("http://localhost:8000/mods/" + modId, {
+      const reponse = await fetch(`${backendBaseUrl}/mods/` + modId, {
             method: "DELETE", mode: "cors", headers: { Authorization: `Bearer ${accessToken}` }
       });
 
@@ -23,7 +27,7 @@ export async function deleteMod(accessToken, modId) {
 }
 
 export async function updateMod(accessToken, modId, formDataMod) {
-      const reponse = await fetch("http://localhost:8000/mods/" + modId, {
+      const reponse = await fetch(`${backendBaseUrl}/mods/` + modId, {
             method: "PUT", mode: "cors", body: JSON.stringify(formDataMod), headers: { Authorization: `Bearer ${accessToken}` }
       });
 
@@ -32,7 +36,7 @@ export async function updateMod(accessToken, modId, formDataMod) {
 }
 
 export async function createMod(accessToken, formDataMod) {
-      const reponse = await fetch("http://localhost:8000/mods/", {
+      const reponse = await fetch(`${backendBaseUrl}/mods/`, {
             method: "POST", mode: "cors", body: JSON.stringify(formDataMod), headers: { Authorization: `Bearer ${accessToken}` }
       });
 
@@ -43,21 +47,21 @@ export async function createMod(accessToken, formDataMod) {
 // Table Modders
 
 export async function getModdersList() {
-      const reponse = await fetch("http://localhost:8000/modders");
+      const reponse = await fetch(`${backendBaseUrl}/modders`);
 
       const data = await reponse.json();
       return data;
 }
 
 export async function getModder(modderId) {
-      const reponse = await fetch("http://localhost:8000/modders/" + modderId);
+      const reponse = await fetch(`${backendBaseUrl}/modders/` + modderId);
 
       const data = await reponse.json();
       return data;
 }
 
 export async function deleteModder(accessToken, modderId) {
-      const reponse = await fetch("http://localhost:8000/modders/" + modderId, {
+      const reponse = await fetch(`${backendBaseUrl}/modders/` + modderId, {
             method: "DELETE", mode: "cors", headers: { Authorization: `Bearer ${accessToken}` }
       });
 
@@ -66,7 +70,7 @@ export async function deleteModder(accessToken, modderId) {
 }
 
 export async function createModder(formDataModder) {
-      const reponse = await fetch("http://localhost:8000/signup/", {
+      const reponse = await fetch(`${backendBaseUrl}/signup/`, {
             method: "POST", mode: "cors", body: JSON.stringify(formDataModder)
       });
 
@@ -75,7 +79,7 @@ export async function createModder(formDataModder) {
 }
 
 export async function updateModder(accessToken, modderId, formDataModder) {
-      const reponse = await fetch("http://localhost:8000/modders/" + modderId, {
+      const reponse = await fetch(`${backendBaseUrl}/modders/` + modderId, {
             method: "PUT", mode: "cors", body: JSON.stringify(formDataModder), headers: { Authorization: `Bearer ${accessToken}` }
       });
 
@@ -84,7 +88,7 @@ export async function updateModder(accessToken, modderId, formDataModder) {
 }
 
 export async function loginModder(formDataModder) {
-      const reponse = await fetch("http://localhost:8000/signin/", {
+      const reponse = await fetch(`${backendBaseUrl}/signin/`, {
             method: "POST", mode: "cors", body: JSON.stringify(formDataModder)
       });
 
