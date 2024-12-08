c4c41a2ff2b4f889e67041ac32be365e67bbcb47
mohuynh <mohuynh@users.noreply.github.com>
2024-12-08 22:30:11 +0100

    Finish login page

--- src/app/components/formModdersLogin.js
+++ src/app/components/formModdersLogin.js
@@ -2,40 +2,79 @@ import { useState } from "react";
 import { loginModder } from "../services/api";
 import { useRouter } from "next/router";
 
+import { Button, Card, Container, Form } from "react-bootstrap";
+import FooterUser from "./footerUser";
+import HeaderUser from "./headerUser";
+
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function FormModderLogin() {
-      const [formDataModder, setFormDataModder] = useState([])
-      const router = useRouter()
+  const [formDataModder, setFormDataModder] = useState([]);
+  const router = useRouter();
 
-      const handleChange = (event) => {
-            const { name, value } = event.target
-            setFormDataModder((prevFormaDataModder) => ({ ...prevFormaDataModder, [name]: value }))
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormDataModder((prevFormaDataModder) => ({
+      ...prevFormaDataModder,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    loginModder(formDataModder).then((result) => {
+      if (!result.hasOwnProperty("message")) {
+        alert("You're connected");
+        localStorage.setItem("jwt-token", result);
+        router.push("/");
+      } else {
+        alert(result["message"]);
       }
+    });
+  };
 
-      const handleSubmit = (event) => {
-            event.preventDefault()
-            loginModder(formDataModder).then((result) => {
-                  if (!result.hasOwnProperty('message')) {
-                        alert("You're connected")
-                        localStorage.setItem("jwt-token", result)
-                        router.push("/")
-                  } else {
-                        alert("Connection error")
-                  }
-            })
-      }
-
-      return <form onSubmit={handleSubmit}>
-            <div>
-                  <label htmlFor="name">Name</label>
-                  <input type="text" id="name" name="name" value={formDataModder.name} onChange={handleChange} />
-            </div>
-            <div>
-                  <label htmlFor="mdp">Mot de passe</label>
-                  <input type="text" id="mdp" name="mdp" value={formDataModder.mdp} onChange={handleChange} />
-            </div>
-            <div>
-                  <button type="submit">Connexion</button>
-            </div>
-      </form>
+  return (
+    <>
+      <header>
+        <HeaderUser />
+      </header>
+      <main>
+        <Container className="mt-4 w-50">
+          <Card>
+            <Card.Header>Login</Card.Header>
+            <Card.Body>
+              <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="name">Name</Form.Label>
+                  <Form.Control
+                    type="text"
+                    id="name"
+                    name="name"
+                    value={formDataModder.name}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3">
+                  <Form.Label htmlFor="mdp">Mot de passe</Form.Label>
+                  <Form.Control
+                    type="password"
+                    id="mdp"
+                    name="mdp"
+                    value={formDataModder.mdp}
+                    onChange={handleChange}
+                  />
+                </Form.Group>
+                <div>
+                  <Button type="submit">Connexion</Button>
+                </div>
+              </Form>
+            </Card.Body>
+          </Card>
+        </Container>
+      </main>
+      <footer className="mt-5">
+        <FooterUser />
+      </footer>
+    </>
+  );
 }
